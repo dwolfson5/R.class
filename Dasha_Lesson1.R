c4e8dfd0a85b29a37dd4b262c6274b0ee977404e
dwolfson5 <66646349+dwolfson5@users.noreply.github.com>
2022-01-17 14:25:44 -0500

    Create Dasha_Lesson1.R

---
+++ Dasha_Lesson1.R
@@ -0,0 +1,8 @@
+a<-"My Name"
+b<- "Dasha."
+c<- paste(a, b, sep= ' is ')
+d<- 20
+e<-2021-d
+f<- paste(c, e, sep= " I was born in ")
+f #Is there a reason to use print(f) instead of just f in the result is the same?
+print(f)
